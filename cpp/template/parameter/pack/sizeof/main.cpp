a7a979c093f7b5692ecf59ad168f2ab1a57ac569
kumhyun-cho <kumhyun.cho@samsung.com>
2021-04-17 01:12:20 +0900

    sizeof...(args) was tested

---
+++ cpp/template/parameter/pack/sizeof/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename ...Ts> void func(Ts... args) {
+    cout << sizeof...(args) << endl;
+}
+
+auto main() -> int {
+    func(1, 2, 3, 4, 5);
+    func("1", "2", "3");
+
+    return 0;
+}
