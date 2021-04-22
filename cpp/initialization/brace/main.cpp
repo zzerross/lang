2980d7038b219c7ca8d6df717ed4e039a4e9b0e2
kumhyun-cho <kumhyun.cho@samsung.com>
2021-04-22 23:12:30 +0900

    brace initialization was tested in delivered class

---
+++ cpp/initialization/brace/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    int a;
+};
+
+class B : public A {
+public:
+    int b;
+};
+
+auto main() -> int {
+    A a = { 1 };
+    cout << a.a << endl << endl;;
+
+#if 201703L <= __cplusplus  // -std=c++17
+    B b = { { 2 }, 3 };
+    cout << b.a << endl;
+    cout << b.b << endl << endl;
+
+    B b1 = { A { 4 }, .b = 5 };
+    cout << b1.a << endl;
+    cout << b1.b << endl << endl;
+
+#if 0 // error: too many initializers for ‘B’
+    B b2 = { .b = 6, A { 7 } };
+    cout << b2.a << endl;
+    cout << b2.b << endl << endl;
+#endif
+#endif
+
+    return 0;
+}
