dd9645757bf4edeff0026a7f15da564edc39e846
kumhyun-cho <kumhyun.cho@samsung.com>
2021-05-01 23:59:06 +0900

    complex plane explains imaginary unit squared

--- c/complex/imaginary/unit/square/main.c
+++ c/complex/imaginary/unit/square/main.c
@@ -2,13 +2,35 @@
 #include <complex.h>
 
 int main() {
-    // imaginary unit square
-    double complex z = I * I;
+    // refer https://www.youtube.com/watch?v=Ko6Ri9zKADY
+    printf(
+        " Imaginary Unit Square  \n"
+        " i ^ 2 = -1             \n"
+        "          ^             \n"
+        "          |             \n"
+        "          + 2i          \n"
+        "          |             \n"
+        "          + i           \n"
+        "          |             \n"
+        " <--+--+--+--+--+-->    \n"
+        "   -2 -1  |  1  2       \n"
+        "          + -i          \n"
+        "          |             \n"
+        "          + -2i         \n"
+        "          |             \n"
+    );
 
+    printf("1 rotates  90 degrees:\n");
+    double complex z = 1 * I;
     double r = creal(z);
     double i = cimag(z);
+    printf("1 * I     = %+02.1lf %+.1lfi\n", r, i);
 
-    printf("I * I = %lf%+lfi\n", r, i);
+    printf("1 rotates 180 degrees:\n");
+    z = 1 * I * I;
+    r = creal(z);
+    i = cimag(z);
+    printf("1 * I * I = %+02.1lf %+.1lfi\n", r, i);
 
     return 0;
 }
