7577880abdc483b9439b87d31ec014c836d4c6cf
kumhyun-cho <kumhyun.cho@samsung.com>
2021-10-16 01:10:05 +0900

    c++ stl set was tested with same pair insersion

--- cpp/stl/set/pair/insert/main.cpp
+++ cpp/stl/set/pair/insert/main.cpp
@@ -12,6 +12,7 @@ int main() {
     scores.insert(Score(20, "Canny"));
     scores.insert(Score(20, "Brown"));
     scores.insert(Score(10, "Army"));
+    scores.insert(Score(10, "Army"));
     scores.insert(Score(30, "Arrow"));
 
     for (const auto& score : scores)
@@ -19,3 +20,12 @@ int main() {
 
     return 0;
 }
+
+#if 0
+$ g++ main.cpp && ./a.out
+
+10 Army
+20 Brown
+20 Canny
+30 Arrow
+#endif
