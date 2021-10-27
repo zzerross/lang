5981dbda5aababc55c0f710e95cf5468ddba043e
kumhyun-cho <kumhyun.cho@samsung.com>
2021-10-28 00:54:48 +0900

    in cpp, enum with no enumerators was tested according to __cpluscplus version

--- cpp/unions/class/no/main.cpp
+++ cpp/unions/class/no/main.cpp
@@ -5,19 +5,30 @@ using namespace std;
 int main() {
     enum class E : int { };
 
-#if 0 // error: cannot convert ‘int’ to ‘main()::E’ in initialization
-    E e2{ 0 };
-#else
-    E e1;
-    cout << static_cast<int>(e1) << endl;
+    cout << "__cplusplus=" << __cplusplus << endl;
+
+    E e0;
+    cout << "e0=" << static_cast<int>(e0) << endl;
+
+#if 201703L < __cplusplus 
+    E e1{ 1 };
+    cout << "e1=" << static_cast<int>(e1) << endl;
 #endif
 
     return 0;
 }
 
 #if 0
-$ g++ main.cpp
-$ ./a.out
+$ g++ main.cpp && ./a.out
+__cplusplus=201402
+e0=0
 
-0
+$ g++ -std=c++17 main.cpp
+__cplusplus=201703
+e0=0
+
+$ g++ -std=c++2a main.cpp
+__cplusplus=201709
+e0=0
+e1=1
 #endif
