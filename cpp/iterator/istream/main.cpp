e6c56a7f4468c18cc75505c3a783208587cf7d7c
kumhyun-cho <kumhyun.cho@samsung.com>
2021-11-04 20:38:56 +0900

    in cpp, accumulate with istream_iterator was tested

---
+++ cpp/iterator/istream/main.cpp
@@ -0,0 +1,27 @@
+#include <numeric>
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+int main() {
+    auto first = istream_iterator<int>(cin);
+    auto last = istream_iterator<int>();
+    int init = 0;
+
+    cout << accumulate(first, last, init) << endl;
+
+    return 0;
+}
+
+#if 0
+$ g++ main.cpp && ./a.out
+1 2 3 4 5 <ctrl+d> 
+15
+
+$ ./a.out
+1
+2
+<ctrl+d> 
+3
+#endif
