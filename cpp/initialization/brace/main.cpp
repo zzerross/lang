34bc961d7072e427b26a9def4f0c233ed73af373
kumhyun-cho <kumhyun.cho@samsung.com>
2022-01-23 02:23:34 +0900

    in cpp, class instance can be initialized with curly brace

--- cpp/initialization/brace/main.cpp
+++ cpp/initialization/brace/main.cpp
@@ -13,17 +13,23 @@ public:
 };
 
 auto main() -> int {
+    A g;
+    cout << "g.a=" << g.a << endl << endl;;
+
+    A z { };
+    cout << "z.a=" << z.a << endl << endl;;
+
     A a = { 1 };
-    cout << a.a << endl << endl;;
+    cout << "a.a=" << a.a << endl << endl;;
 
 #if 201703L <= __cplusplus  // -std=c++17
     B b = { { 2 }, 3 };
-    cout << b.a << endl;
-    cout << b.b << endl << endl;
+    cout << "b.a=" << b.a << endl;
+    cout << "b.b=" << b.b << endl << endl;
 
     B b1 = { A { 4 }, .b = 5 };
-    cout << b1.a << endl;
-    cout << b1.b << endl << endl;
+    cout << "b1.a=" << b1.a << endl;
+    cout << "b1.b=" << b1.b << endl << endl;
 
 #if 0 // error: too many initializers for ‘B’
     B b2 = { .b = 6, A { 7 } };
@@ -34,3 +40,19 @@ auto main() -> int {
 
     return 0;
 }
+
+#if 0
+$ g++ -std=c++17 main.cpp
+$ ./a.out
+g.a=32616
+
+z.a=0
+
+a.a=1
+
+b.a=2
+b.b=3
+
+b1.a=4
+b1.b=5
+#endif
